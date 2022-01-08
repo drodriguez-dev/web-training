3a413d120220dfcf251e0a029dfc13397e6e0f3b
drodriguez <drodriguez@esdelogica.es>
2022-01-08 23:23:29 +0100

    Adding modal windows and pop overs

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/**
+ * Logs a message to the console ({@link console.log()}).
+ * 
+ * @param {string} message Message to be logged to the console
+ */
+function writeToConsole(message) {
+  console.log("A message: %s", message);
+}
