de8ff31f04200236dab8dfa408ca5f176f3d76fe
drodriguez <drodriguez@esdelogica.es>
2022-01-09 12:10:05 +0100

    Solve issue with bootstrap support with sass

--- js/main.js
+++
@@ -1,8 +0,0 @@
-/**
- * Logs a message to the console ({@link console.log()}).
- * 
- * @param {string} message Message to be logged to the console
- */
-function writeToConsole(message) {
-  console.log("A message: %s", message);
-}
